fda1edaa0b06c3262a28128146e8dd4c19f997ab
Biswajeet Sahoo <144132188+Bsahoo99@users.noreply.github.com>
2026-07-20 17:30:21 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.07
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "Bissu"
+print(type(variable))
+variable = [10,11,12]
+print(type(variable))
